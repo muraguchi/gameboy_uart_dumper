82074a9ec33b20d3165b6c478c652dd00759e957
Takanobu MURAGUCHI <muraguchi@gmail.com>
2020-01-02 07:47:52 +0900

    Added dumper scripts

---
+++ py/read_rom.py
@@ -0,0 +1,238 @@
+#! /usr/bin/python
+import serial
+import struct
+
+ser = serial.Serial('/dev/ttyUSB0',19200,timeout=None)
+outfile="NONAME.gb"
+
+# Get_cart_type
+ser.write (("R1%04X" %( 0x0143)).encode() )
+cgb_flag = int(ser.read(2).decode(),16)
+
+# Get title
+title_length=16
+if (cgb_flag==0x80 or cgb_flag == 0xC0):
+    title_length=15
+
+title=""
+for index in range(title_length):
+    ser.write (("R1%04X" %( index+0x134)).encode() )
+    title+=chr(int(ser.read(2).decode(),16))
+
+    
+# Get cart_type
+
+ser.write (("R1%04X" %( 0x0147)).encode() )
+cart_type = int(ser.read(2).decode(),16)
+
+# Get ROM size
+
+ser.write (("R1%04X" %( 0x0148)).encode() )
+rom_size = int(ser.read(2).decode(),16)
+
+# Get RAM size
+
+ser.write (("R1%04X" %( 0x0148)).encode() )
+ram_size = int(ser.read(2).decode(),16)
+
+
+print("Title: %s" % (title))
+
+if ( cgb_flag==0x80 ):
+    print("CGB flag: Game Boy / Game Boy Color")
+elif ( cgb_flag==0xC0 ):
+    print("CGB flag: Game Boy Color")
+else:
+    print("CGB flag: Game Boy")
+
+if ( cart_type==0x00):
+    print("Cartrige type: ROM ONLY")
+elif ( cart_type==0x01):
+    print("Cartrige type: MBC1")
+elif ( cart_type==0x02):
+    print("Cartrige type: MBC1+RAM")
+elif ( cart_type==0x03):
+    print("Cartrige type: MBC1+RAM+BATTERY")
+elif ( cart_type==0x05):
+    print("Cartrige type: MBC2")
+elif ( cart_type==0x06):
+    print("Cartrige type: MBC2+BATTERY")
+elif ( cart_type==0x0F):
+    print("Cartrige type: MBC3+TIMER+BATTERY")
+elif ( cart_type==0x10):
+    print("Cartrige type: MBC3+TIMER+RAM+BATTERY")
+elif ( cart_type==0x11):
+    print("Cartrige type: MBC3")
+elif ( cart_type==0x12):
+    print("Cartrige type: MBC3+RAM")
+elif ( cart_type==0x13):
+    print("Cartrige type: MBC3+RAM+BATTERY")
+elif ( cart_type==0x19):
+    print("Cartrige type: MBC5")
+elif ( cart_type==0x1A):
+    print("Cartrige type: MBC5+RAM")
+elif ( cart_type==0x1B):
+    print("Cartrige type: MBC5+RAM+BATTERY")
+elif ( cart_type==0x1C):
+    print("Cartrige type: MBC5+RUMBLE")
+elif ( cart_type==0x1D):
+    print("Cartrige type: MBC5+RUMBLE+RAM")
+elif ( cart_type==0x1E):
+    print("Cartrige type: MBC5+RUMBLE+RAM+BATTERY")
+elif ( cart_type==0x22):
+    print("Cartrige type: MBC7+SENSOR+RAM+BATTERY")
+elif ( cart_type==0xFD):
+    print("Cartrige type: TAMA5")
+elif ( cart_type==0xFE):
+    print("Cartrige type: HuC3")
+elif ( cart_type==0xFF):
+    print("Cartrige type: HuC1")
+else:
+    print("Cartrige type: 0x%02X" % (cart_type))
+
+if (rom_size==0x00):
+    print("ROM size: 32 KBytes")
+elif (rom_size==0x01):
+    print("ROM size: 64 KBytes / 4 banks")
+elif (rom_size==0x02):
+    print("ROM size: 128 KBytes / 8 banks")
+elif (rom_size==0x03):
+    print("ROM size: 256 KBytes / 16 banks")
+elif (rom_size==0x04):
+    print("ROM size: 512 KBytes / 32 banks")
+elif (rom_size==0x05):
+    print("ROM size: 1 MBytes / 64 banks")
+elif (rom_size==0x06):
+    print("ROM size: 2 MBytes / 128 banks")
+elif (rom_size==0x07):
+    print("ROM size: 4 MBytes / 256 banks")
+elif (rom_size==0x08):
+    print("ROM size: 8 MBytes / 512 banks")
+elif (rom_size==0x52):
+    print("ROM size: 1.1 MBytes / 72 banks")
+elif (rom_size==0x53):
+    print("ROM size: 1.2 MBytes / 80 banks")
+elif (rom_size==0x54):
+    print("ROM size: 1.5 MBytes / 96 banks")
+else: 
+    print("ROM size: 0x%02X" %(rom_size))
+
+if (ram_size==0x00):
+    print("RAM size: None")
+elif (ram_size==0x01):
+    print("RAM size: 2 Kbytes")
+elif (ram_size==0x02):
+    print("RAM size: 8 Kbytes")
+elif (ram_size==0x03):
+    print("RAM size: 32 Kbytes / 4 banks")
+elif (ram_size==0x04):
+    print("RAM size: 128 Kbytes / 16 banks")
+elif (ram_size==0x05):
+    print("RAM size: 64 Kbytes / 8 banks")
+else:
+    print("RAM size: 0x%02X" %(ram_size))
+
+
+
+outfile=title.replace(".","_").replace(" ","_").replace('\x00','').replace('.','_').replace('/','_').replace("\'",'_')+".gb"
+for i in range(16):
+    outfile=outfile.replace("__","_")
+outfile=outfile.replace("_.gb",".gb")
+print(outfile)
+gbf=open((outfile),"wb")
+
+#NO ROM
+if (cart_type==0x00):
+    if(rom_size==0x00):
+        for ra in range(0x8000):
+           if(ra % 0x4000 == 0): 
+               ser.write (("P1%02X" %( ra>>8 ) ).encode() )
+           data = int(ser.read(2).decode(),16)
+           gbf.write(struct.pack("B", data ))
+           if (ra%16==0):
+               print(("0x00 0x%04X: %02X " % (ra,data)) , end="")
+           elif (ra%16==15):
+               print("%02X" % data)
+           else:
+               print(("%02X " % data), end="")
+#MBC
+else :
+    if (rom_size==0x01):
+        num_banks=4
+    elif (rom_size==0x02):
+        num_banks=8
+    elif (rom_size==0x03):
+        num_banks=16
+    elif (rom_size==0x04):
+        num_banks=32
+    elif (rom_size==0x05):
+        num_banks=64
+    elif (rom_size==0x06):
+        num_banks=128
+    elif (rom_size==0x07):
+        num_banks=256
+    elif (rom_size==0x08):
+        num_banks=512
+    for bank_index in range(num_banks):
+        if (bank_index==0):
+            for ra in range(0x4000):
+                if(ra % 0x4000 == 0): 
+                    ser.write (("P1%02X" %( ra>>8 ) ).encode() )
+                #ser.write (("R1%04X" %( ra ) ).encode() )
+                data = int(ser.read(2).decode(),16)
+                gbf.write(struct.pack("B", data ))
+                if (ra%16==0):
+                    print(("0x000 0x%04X: %02X " % (ra,data)) , end="")
+                elif (ra%16==15):
+                    print("%02X" % data)
+                else:
+                    print(("%02X " % data), end="")
+        else:
+            # Change BANK
+            # MBC1
+            if (cart_type==0x01 or cart_type==0x02 or cart_type==0x03 ):
+                # Upper 2bit
+                ser.write (("W14100%02X" %((bank_index>>5)&0x3) ).encode())
+                data = int(ser.read(2).decode(),16)
+                # Lower 5bit
+                ser.write (("W12100%02X" %(bank_index&0x1F) ).encode() )
+                data = int(ser.read(2).decode(),16)
+            # MBC2
+            elif (cart_type==0x05 or cart_type==0x06):
+                ser.write (("W12100%02X" %(bank_index&0xf) ).encode())
+                data = int(ser.read(2).decode(),16)
+            # MBC3
+            elif  (cart_type==0x0f or cart_type==0x10 or cart_type==0x11 or cart_type==0x12 or cart_type==0x13 ):
+                ser.write (("W12000%02X" %(bank_index) ).encode())
+                data = int(ser.read(2).decode(),16)
+            # MBC5 or MBC7
+            elif(cart_type==0x19 or cart_type==0x1a or cart_type==0x1b or cart_type==0x1c or cart_type==0x1d or cart_type==0x1e or cart_type==0x22):
+                # Upper 1bit
+                ser.write (("W13100%02X" %((bank_index>>8)&0x1) ).encode())
+                data = int(ser.read(2).decode(),16)
+                # Lower 8bit
+                ser.write (("W12100%02X" %(bank_index&0xff) ).encode())
+                data = int(ser.read(2).decode(),16)
+            # HuC1 or TAMA5
+            elif(cart_type==0xfe or cart_type==0xff):
+                #  6bit
+                ser.write (("W12100%02X" %(bank_index&0x3F) ).encode() )
+                data = int(ser.read(2).decode(),16)
+            print("Change Bank 0x%03X (%d) "%(bank_index,bank_index))
+            for ra in range(0x4000,0x8000):
+                if(ra % 0x4000 == 0): 
+                    ser.write (("P1%02X" %( ra>>8 ) ).encode() )
+                #ser.write (("R1%04X" %( ra ) ).encode() )
+                data = int(ser.read(2).decode(),16)
+                gbf.write(struct.pack("B", data ))
+                if (ra%16==0):
+                    print(("0x%03X 0x%04X: %02X " % (bank_index,ra,data)) , end="")
+                elif (ra%16==15):
+                    print("%02X" % data)
+                else:
+                    print(("%02X " % data), end="")
+
+
+
+    
+    
---
+++ py/verify_sum.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+import sys
+
+if (len(sys.argv)<=1):
+    print ("Usage: %s gameboy_romfile.gb" % sys.argv[0] )
+    exit()
+else:
+    fgb = open (sys.argv[1],"rb")
+    print ("Verify %s" % sys.argv[1])
+    # compare header checksum
+
+    # get header checksum at 0x14d point
+    fgb.seek(0x14d)
+    header_hc=int.from_bytes(fgb.read(1),'little')
+    calc_hc = 0
+    for addr in range(0x134,0x14C+1):
+        fgb.seek(addr)
+        calc_hc = ( calc_hc - (int.from_bytes(fgb.read(1),'little')) - 1 ) % 0x100
+    #print ("Header Checksum at 0x14d : 0x%02X" % header_hc)
+    #print ("Header Checksum calculated : 0x%02X" % calc_hc)
+    if (calc_hc==header_hc):
+        print ("HEADER CHECKSUM COMPARE RESULT : OK")
+    else:
+        print ("HEADER CHECKSUM COMPARE RESULT : ERROR")
+    
+    # compare glocal checksum
+    fgb.seek(0,2)
+    last_addr = fgb.tell()
+    fgb.seek(0x14e)
+    header_gc = int.from_bytes(fgb.read(2),'big')
+    calc_gc=0
+
+    for addr in range(0,last_addr+1):
+        if(addr==0x14e or addr==0x14f):
+            continue
+        else:
+            fgb.seek(addr)
+            calc_gc = ( (calc_gc + int.from_bytes(fgb.read(1),'little')) % 0x10000)
+    print ("Global Checksum at 0x14E:0x14F 0x%04X" % header_gc)
+    print ("Global Checksum calculated : 0x%04X" % calc_gc)
+    if (calc_gc==header_gc):
+        print ("GLOBAL CHECKSUM COMPARE RESULT : OK")
+    else:
+        print ("GLOBAL CHECKSUM COMPARE RESULT : ERROR")
